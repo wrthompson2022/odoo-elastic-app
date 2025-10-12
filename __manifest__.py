7c06c5aa898379eedf09d22144daa2fa798cd536
wrthompson2022 <99107098+wrthompson2022@users.noreply.github.com>
2025-10-12 10:03:15 -0400

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Elastic Integration (SFTP)",
+    "summary": "Manage Elastic SFTP exports/imports.",
+    "version": "18.0.1.0.0",
+    "category": "Integration",
+    "author": "P2 Business Solutions",
+    "license": "LGPL-3",
+    "depends": ["base","sale_management","stock","contacts","product","mail"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/menu.xml"
+    ],
+    "application": True,
+}
